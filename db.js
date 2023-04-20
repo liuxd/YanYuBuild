44ca7b12618ce71e51908f8beee173b6217597cf
allen <allen.liu@taxtraders.co.nz>
2023-04-20 22:32:18 +1200

    init

---
+++ db.js
@@ -0,0 +1,360 @@
+let db = {
+    hero: [
+        {
+            name: '孟翔',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '薛龙城',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '拜妮娜',
+            gender: 'male',
+            type: 'limited',
+        },
+        {
+            name: '南知意',
+            gender: 'male',
+            type: 'limited',
+        },
+        {
+            name: '萧佩吟',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '商昊乾',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '沙孤兰',
+            gender: 'male',
+            type: 'limited',
+        },
+        {
+            name: '巫灵儿',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '李一尘',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '月蓉',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '苏念雪',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '柳如意',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '钟无月',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '丁小飞',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '凌烟',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '火工老僧',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '姜逸尘',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '陈元封',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '田鹏',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '秦无双',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '厉若海',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '杜月寒',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '丐武圣',
+            gender: 'male',
+            type: 'paid',
+        },
+        {
+            name: '桃花',
+            gender: 'male',
+            type: 'limited',
+        },
+        {
+            name: '陶渊溢',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '拓拔浚',
+            gender: 'male',
+            type: 'free',
+        },
+        {
+            name: '夜无幽',
+            gender: 'male',
+            type: 'limited',
+        },
+        {
+            name: '杨延轩',
+            gender: 'male',
+            type: 'paid',
+        }
+    ],
+    skill: [
+        {
+            name: '烈阳神掌',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '五毒神掌',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '玄阴掌',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '凝血神爪',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '密宗大手印',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '醉梦神拳',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '大奔雷手',
+            type: 'attack',
+            subtype: 'fist',
+        },
+        {
+            name: '镇派',
+            type: 'attack',
+            subtype: 'sword',
+        },
+        {
+            name: '五狱剑诀',
+            type: 'attack',
+            subtype: 'sword',
+        },
+        {
+            name: '回风舞柳剑',
+            type: 'attack',
+            subtype: 'sword',
+        },
+        {
+            name: '西子君剑',
+            type: 'attack',
+            subtype: 'sword',
+        },
+        {
+            name: '太虚神悟刀',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '破锋八斩',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '绣春刀诀',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '伏羲刀法',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '心音流刀术',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '寂刀诀',
+            type: 'attack',
+            subtype: 'saber',
+        },
+        {
+            name: '烛龙枪诀',
+            type: 'attack',
+            subtype: 'pike',
+        },
+        {
+            name: '无瑕棍法',
+            type: 'attack',
+            subtype: 'pike',
+        },
+        {
+            name: '凌霜枪法',
+            type: 'attack',
+            subtype: 'pike',
+        },
+        {
+            name: '荡魔枪诀',
+            type: 'attack',
+            subtype: 'pike',
+        },
+        {
+            name: '龙象般若功',
+            type: 'magic',
+            subtype: 'sun',
+        },
+        {
+            name: '离火诀',
+            type: 'magic',
+            subtype: 'sun',
+        },
+        {
+            name: '天一神功',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '碧波神功',
+            type: 'magic',
+            subtype: 'moon',
+        },
+        {
+            name: '步天歌诀',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '玄冥经',
+            type: 'magic',
+            subtype: 'moon',
+        },
+        {
+            name: '太玄真经',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '乾元功',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '血魔经',
+            type: 'magic',
+            subtype: 'moon',
+        },
+        {
+            name: '万象魔功',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '长生诀',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '青蛾功',
+            type: 'magic',
+            subtype: 'sun',
+        },
+        {
+            name: '净尘琉璃体',
+            type: 'magic',
+            subtype: 'mixed',
+        },
+        {
+            name: '千回百转',
+            type: 'fly',
+        },
+        {
+            name: '碎星步',
+            type: 'fly',
+        },
+        {
+            name: '万里神行',
+            type: 'fly',
+        },
+        {
+            name: '鬼影迷踪',
+            type: 'fly',
+        },
+        {
+            name: '云龙三折',
+            type: 'fly',
+        },
+        {
+            name: '凌波微步',
+            type: 'fly',
+        },
+        {
+            name: '幽行步',
+            type: 'fly',
+        },
+        {
+            name: '身空行',
+            type: 'fly',
+        },
+        {
+            name: '天元落',
+            type: 'fly',
+        },
+        {
+            name: '天魔舞',
+            type: 'fly',
+        },
+        {
+            name: '追魂步',
+            type: 'fly',
+        },
+    ]
+};
